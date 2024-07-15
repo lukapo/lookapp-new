8adcd8121923b795f79c2c2f78d39708cc877278
Luka Postruzin <luka.postruzin@ericsson.com>
2024-07-15 09:39:14 +0200

    New inflatable (Big House) added

--- src/data/inflatables.jsx
+++ src/data/inflatables.jsx
@@ -1,11 +1,11 @@
 export const inflatables = [
     {
         id: 1,
-        name: "Dvorac", 
+        name: "Dvorac",
         description:
-            <>Oduševljava one najmlađe.<br /> Idealan za djecu vrtićke dobi. <br />Ima prostor za skakanje, tunel za provlačenje, posebni odjeljak s lopticama i tobogan visine 2.5m. <br />Za unutarnji i vanjski prostor.</>,
+            <>Oduševljava one najmlađe.<br /> Idealan za djecu vrtićke dobi (max 6 godina). <br />Ima prostor za skakanje, tunel za provlačenje, posebni odjeljak s lopticama i tobogan visine 2.5m. <br />Za unutarnji i vanjski prostor.</>,
         price: 40,
-        dimensions: "D x Š x V = (485 x 320 x 295) cm",
+        dimensions: "D x Š x V = (485 x 320 x 265) cm",
         extraInfo:
             "Navedena cijena je za samopreuzimanje. Imamo opciju dostave koja se dodatno naplaćuje, a cijena dostave ovisi o udaljenosti.",
         images: [
@@ -18,19 +18,36 @@ export const inflatables = [
     },
     {
         id: 2,
-        name: "Minioni", 
+        name: "Velika kuća",
+        description:
+            <>Kao i naš Dvorac, oduševljava one najmlađe.<br /> Idealan za djecu vrtićke dobi (max 6 godina). <br />Zatvoren prostor za skakanje, tunel za provlačenje, pokrov kod vrućina, poseban, veći odjeljak s lopticama i tobogan. <br />Za unutarnji i vanjski prostor.</>,
+        price: 50,
+        dimensions: "D x Š x V = (455 x 330 x 265) cm",
+        extraInfo:
+            "Navedena cijena je za samopreuzimanje. Imamo opciju dostave koja se dodatno naplaćuje, a cijena dostave ovisi o udaljenosti.",
+        images: [
+            "./images/Kuca1.jpg",
+            "./images/Kuca2.jpg",
+            "./images/Kuca3.jpg",
+            "./images/Kuca4.jpg"
+        ],
+        logoImage: "./images/house.png"
+    },
+    {
+        id: 3,
+        name: "Minioni",
         description:
             <>Idealan za djecu do 12 godina, ali i za one starije. <br />Uz tobogan, ima i prostor za skakanje pored kojeg je zaštita od izletavanja.</>,
         price: 80,
         dimensions: "D x Š x V = cca (650 x 400 x 600) cm.",
         extraInfo:
             "Navedena cijena je za samopreuzimanje. Imamo opciju dostave koja se dodatno naplaćuje, a cijena dostave ovisi o udaljenosti.",
-            images: [
-                "./images/Minioni1.jpg",
-                "./images/Minioni2.jpg",
-                "./images/Minioni3.jpg",
-                "./images/Minioni4.jpg"
-            ],
-            logoImage: "./images/minions-logo.png"
+        images: [
+            "./images/Minioni1.jpg",
+            "./images/Minioni2.jpg",
+            "./images/Minioni3.jpg",
+            "./images/Minioni4.jpg"
+        ],
+        logoImage: "./images/minions-logo.png"
     },
 ];
