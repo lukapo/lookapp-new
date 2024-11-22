ad80a1bd1f9396ff1e52c4c370ecb627d8fe94f9
Luka Postruzin <luka.postruzin@ericsson.com>
2024-11-22 18:57:47 +0100

    New pic for new inflatable

--- src/data/inflatables.jsx
+++ src/data/inflatables.jsx
@@ -3,11 +3,11 @@ export const inflatables = [
         id: 1,
         name: "Dvorac",
         description:
-            <>Oduševljava one najmlađe.<br /> Idealan za djecu vrtićke dobi (max 6 godina). <br />Ima prostor za skakanje, tunel za provlačenje, posebni odjeljak s lopticama i tobogan visine 2.5m. <br />Za unutarnji i vanjski prostor.</>,
+        <>Oduševljava one najmlađe.<br /> Idealan za djecu vrtićke dobi (max 6 godina). <br />Ima prostor za skakanje, tunel za provlačenje, posebni odjeljak s lopticama i tobogan visine 2.5m. <br />Za unutarnji i vanjski prostor.</>,
         price: 50,
         dimensions: "D x Š x V = (485 x 320 x 265) cm",
         extraInfo:
-            "Navedena cijena je za samopreuzimanje. Imamo opciju dostave koja se dodatno naplaćuje, a cijena dostave ovisi o udaljenosti.",
+        "Navedena cijena je za samopreuzimanje. Imamo opciju dostave koja se dodatno naplaćuje, a cijena dostave ovisi o udaljenosti.",
         images: [
             "./images/Dvorac1.jpg",
             "./images/Dvorac2.jpeg",
@@ -18,13 +18,30 @@ export const inflatables = [
     },
     {
         id: 2,
+        name: "Tranformator",
+        description:
+            <>Idealan za djecu do 12 godina, ali i za one starije. <br />Veliki prostor za skakanje, zaštitna mreža, ograda, tobogan - ima sve. Uz nadoplatu, na njega možemo staviti <strong>natpis ili temu po želji </strong>uz prethodnu najavu.</>,
+        price: 80,
+        dimensions: "D x Š x V = cca (500 x 500 x 300) cm.",
+        extraInfo:
+            "Navedena cijena je za samopreuzimanje. Imamo opciju dostave koja se dodatno naplaćuje, a cijena dostave ovisi o udaljenosti.",
+        images: [
+            "./images/CB1.jpg",
+            "./images/CB2.jpg",
+            "./images/CB3.jpg",
+            "./images/CB4.jpg"
+        ],
+        logoImage: "./images/CB-logo.png"
+    },
+    {
+        id: 3,
         name: "Velika kuća",
         description:
-            <>Kao i naš Dvorac, oduševljava one najmlađe.<br /> Idealan za djecu vrtićke dobi (max 6 godina). <br />Zatvoren prostor za skakanje, tunel za provlačenje, pokrov kod vrućina, poseban, veći odjeljak s lopticama i tobogan. <br />Za unutarnji i vanjski prostor.</>,
+        <>Kao i naš Dvorac, oduševljava one najmlađe.<br /> Idealan za djecu vrtićke dobi (max 6 godina). <br />Zatvoren prostor za skakanje, tunel za provlačenje, pokrov kod vrućina, poseban, veći odjeljak s lopticama i tobogan. <br />Za unutarnji i vanjski prostor.</>,
         price: 50,
         dimensions: "D x Š x V = (455 x 330 x 265) cm",
         extraInfo:
-            "Navedena cijena je za samopreuzimanje. Imamo opciju dostave koja se dodatno naplaćuje, a cijena dostave ovisi o udaljenosti.",
+        "Navedena cijena je za samopreuzimanje. Imamo opciju dostave koja se dodatno naplaćuje, a cijena dostave ovisi o udaljenosti.",
         images: [
             "./images/Kuca1.jpg",
             "./images/Kuca2.jpg",
@@ -34,7 +51,7 @@ export const inflatables = [
         logoImage: "./images/house.png"
     },
     {
-        id: 3,
+        id: 4,
         name: "Minioni",
         description:
             <>Idealan za djecu do 12 godina, ali i za one starije. <br />Uz tobogan, ima i prostor za skakanje pored kojeg je zaštita od izletavanja.</>,
@@ -49,22 +66,5 @@ export const inflatables = [
             "./images/Minioni4.jpg"
         ],
         logoImage: "./images/minions-logo.png"
-    },
-    {
-        id: 4,
-        name: "Crveno-plavi",
-        description:
-            <>Idealan za djecu do 12 godina, ali i za one starije. <br />Veliki prostor za skakanje, zaštitna mreža, ograda, tobogan - ima sve. Uz nadoplatu, na njega možemo staviti natpis ili temu po želji uz prethodnu najavu.</>,
-        price: 80,
-        dimensions: "D x Š x V = cca (500 x 500 x 300) cm.",
-        extraInfo:
-            "Navedena cijena je za samopreuzimanje. Imamo opciju dostave koja se dodatno naplaćuje, a cijena dostave ovisi o udaljenosti.",
-        images: [
-            "./images/CB1.jpg",
-            "./images/CB2.jpg",
-            "./images/CB3.jpg",
-            "./images/CB4.jpg"
-        ],
-        logoImage: "./images/CB-logo.png"
     },
 ];
