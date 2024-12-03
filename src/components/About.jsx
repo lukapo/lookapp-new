aa3980a20fcfd24f93c0cd8a18d71390bc65d572
Luka Postruzin <luka.postruzin@ericsson.com>
2024-12-03 12:41:29 +0100

    Transitions and tranforms added to images and map

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,33 +3,34 @@ import '../css/About.css';
 const About = () => {
   return (
     <>
-    <SectionDivider title="O nama..." id="about" />
-    <section className="about-section">
-         
+      <SectionDivider title="O nama..." id="about" />
+      <section className="about-section">
+
         <p>
-            Primarna djelatnost obrta LookApp su poduke i savjetovanje.</p>
-            <p>
-            Držimo poduke vezane uz Microsoft Office paket (Excel, Word, Powerpoint) zaposlenicima tvrtki i
-            privatno, predajemo znanje o frontend
-            programiranju (HTML, CSS, JavaScript, Git, ReactJS),
-            programskom jeziku Java te dajemo instrukcije iz raznih predmeta osnovnih i srednjih škola.
+          Primarna djelatnost obrta LookApp su poduke i savjetovanje.</p>
+        <p>
+          Držimo poduke vezane uz Microsoft Office paket (Excel, Word, Powerpoint) zaposlenicima tvrtki i
+          privatno, predajemo znanje o frontend
+          programiranju (HTML, CSS, JavaScript, Git, ReactJS),
+          programskom jeziku Java te dajemo instrukcije iz raznih predmeta osnovnih i srednjih škola.
         </p>
         <p>
-            Također, savjetujemo klijente kako napisati kvalitetan životopis i molbu kod apliciranja za posao i
-            pomažemo im u pisanju istih, ispravljamo gramatičke greške kako na hrvatskom, tako i na engleskom
-            jeziku.
+          Također, savjetujemo klijente kako napisati kvalitetan životopis i molbu kod apliciranja za posao i
+          pomažemo im u pisanju istih, ispravljamo gramatičke greške kako na hrvatskom, tako i na engleskom
+          jeziku.
         </p>
         <p>
 
-            Uz navedeno, iznajmljujemo tobogane i dvorce na
-            napuhavanje i uživamo u svakom dobro obavljenom poslu.
+          Uz navedeno, iznajmljujemo tobogane i dvorce na
+          napuhavanje i uživamo u svakom dobro obavljenom poslu.
 
         </p>
-        <div style={{textAlign: "center"}}>
-        <p className="last-paragraph"><em>Obratite nam se s povjerenjem.</em></p>
-        <img className="business-pic" src="./images/business.jpg" alt="Tabla obrta LookApp" width="50%" />
+        <div style={{ textAlign: "center" }}>
+          <p className="last-paragraph"><em>Obratite nam se s povjerenjem.</em></p>
+          <div className="business-pic-container"><a href="https://g.co/kgs/78cd3jB"><img className="business-pic" src="./images/business.jpg" alt="Tabla obrta LookApp"/></a>
+          </div>
         </div>
-    </section>
+      </section>
     </>
   );
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,48 +1,50 @@
 import SectionDivider from "./SectionDivider";
 import '../css/Contact.css';
 const Contact = () => {
-  return (
-    <>
-    <SectionDivider title="Kontakt" id="contact"/>
-    <section className="contact-section">
-            <a title="WhatsApp" href="https://wa.me/385913653328">
-                <img src="./images/whatsapp-black-logo-icon--24.png" alt="WhatsApp ikona" width="40px" />
-                <em>WhatsApp</em>
-            </a>
-            <hr />
-            
-            <a title="Nazovite nas" href="tel:+385913653328">
-                <img src="./images/white-phone-icon-png--clipart-best-30.png" alt="Telephone ikona" width="40px" />
-                <em>+385913653328</em>
-            </a>
-            <hr />
-            
-            <a title="Pošaljite mail" href="mailto:lookapp200@gmail.com">
-                <img className="img2" src="./images/mail.png" alt="Mail ikona" width="40px"/>
-                <em>luka.postruzin9@gmail.com</em>
-            </a>
-            <hr />
-            <a title="Javite nam se na društvenim mrežama" className="social" href="https://www.facebook.com/obrtLookApp" target="_blank" rel="noreferrer">
-                <div>
-                    <img src="./images/clipart735576.png" alt="Facebook ikona" width="40px" />
-                    <img src="./images/instagram-icon.png" alt="Social ikona" width="40px"/>
-                </div>
-            </a>
-    </section>
-    <hr/>
-    <section className="map-section">
-        <header>
-            <h3>Kako do nas?</h3>
-        </header>
-        <iframe className="map"
-            src="https://www.google.com/maps/embed?pb=!1m21!1m12!1m3!1d2772.600365326565!2d15.910143520538687!3d45.97924092648918!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m6!3e0!4m0!4m3!3m2!1d45.9795727!2d15.911597299999999!5e0!3m2!1sen!2shr!4v1686912731079!5m2!1sen!2shr"
-             loading="lazy" referrerPolicy="no-referrer-when-downgrade">
-        
-        </iframe>
-    </section>
+    return (
+        <>
+            <SectionDivider title="Kontakt" id="contact" />
+            <section className="contact-section">
+                <a title="WhatsApp" href="https://wa.me/385913653328">
+                    <img src="./images/whatsapp-black-logo-icon--24.png" alt="WhatsApp ikona" width="40px" />
+                    <em>WhatsApp</em>
+                </a>
+                <hr />
 
-</>
-  )
+                <a title="Nazovite nas" href="tel:+385913653328">
+                    <img src="./images/white-phone-icon-png--clipart-best-30.png" alt="Telephone ikona" width="40px" />
+                    <em>+385913653328</em>
+                </a>
+                <hr />
+
+                <a title="Pošaljite mail" href="mailto:lookapp200@gmail.com">
+                    <img className="img2" src="./images/mail.png" alt="Mail ikona" width="40px" />
+                    <em>luka.postruzin9@gmail.com</em>
+                </a>
+                <hr />
+                <a title="Javite nam se na društvenim mrežama" className="social" href="https://www.facebook.com/obrtLookApp" target="_blank" rel="noreferrer">
+                    <div>
+                        <img src="./images/clipart735576.png" alt="Facebook ikona" width="40px" />
+                        <img src="./images/instagram-icon.png" alt="Social ikona" width="40px" />
+                    </div>
+                </a>
+            </section>
+            <hr />
+            <section className="map-section">
+                <header>
+                    <h3>Kako do nas?</h3>
+                </header>
+                <div>
+                    <iframe className="map"
+                        src="https://www.google.com/maps/embed?pb=!1m21!1m12!1m3!1d2772.600365326565!2d15.910143520538687!3d45.97924092648918!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m6!3e0!4m0!4m3!3m2!1d45.9795727!2d15.911597299999999!5e0!3m2!1sen!2shr!4v1686912731079!5m2!1sen!2shr"
+                        loading="lazy" referrerPolicy="no-referrer-when-downgrade">
+
+                    </iframe>
+                </div>
+            </section>
+
+        </>
+    )
 }
 
 export default Contact
